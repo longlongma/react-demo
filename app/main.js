a7fe3358de040f94960dea77cfe8fd9febfbad33
AngelaMa <angelama@mintcode.com>
2017-11-02 17:10:58 +0800

    功搭建项目

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Component1 from './components/Component1.jsx';
-ReactDom.render(
+ReactDOM.render(
     <Component1 />,
     document.getElementById('content')
 )
