c787f791a024591de726e89074b6f75082f69484
angela <mllabey@163.com>
2017-12-06 22:13:41 +0800

    引入antd

--- app/components/reactToDoList/ToDoListArea.js
+++ app/components/reactToDoList/ToDoListArea.js
@@ -1,11 +1,15 @@
 import React,{PropTypes} from 'react';
 import ToDoList from "./ToDoList";
 import ToDoInput from "./ToDoInput";
+import 'antd/dist/antd.css'; 
 export default class ToDoListArea extends React.Component{
+    constructor(){
+        super();
+    }
     render(){
         return(
             <div className="todolist-area">
-               <ToDoList/>    
+               <ToDoList />    
                <ToDoInput/> 
             </div>
         )
