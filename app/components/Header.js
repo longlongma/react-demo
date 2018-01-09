c42d553cbae19c43205558b86b31e77b3ba86890
angela <mllabey@163.com>
2018-01-09 21:53:04 +0800

    menu

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,6 +5,10 @@ export default class Header extends React.Component{
       <div>
         <div className="header">
           <h3 className="title">react练习</h3>
+          <ul className="header_menu">
+            <li>ToDoList</li>
+            <li>Redux</li>
+          </ul>
         </div>
       </div>
     )
